13725fae1983cd3a4ca4daaa9591eb977d9d8c47
Basilia Dupp <bdupp@student.21-school.ru>
2023-08-11 11:19:26 +0700

    Initial Commit

---
+++ CleanNotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanNotesApp
+//
+//  Created by Анна Вертикова on 11.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
